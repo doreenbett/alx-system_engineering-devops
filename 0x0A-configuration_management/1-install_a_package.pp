4fbbf8003731c6b3f9d2b5a5885d3a1616a06850
doreenbett <88307192+doreenbett@users.noreply.github.com>
2022-06-09 22:21:15 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Install flask,version 2.1.0
+# Installs puppet-lint
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
